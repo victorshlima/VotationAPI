e12d1fc581e79dbbf0dd5d01475ad96b2a3d58db
Victation <victorshlima@gmail.com>
2019-11-29 02:42:26 -0300

    Heroku Deploy OK

--- src/main/java/com/cooperativeX/votation/restvote/domain/Agenda.java
+++ src/main/java/com/cooperativeX/votation/restvote/domain/Agenda.java
@@ -1,8 +1,5 @@
 package com.cooperativeX.votation.restvote.domain;
-
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import javax.persistence.*;
 import java.util.List;
 
